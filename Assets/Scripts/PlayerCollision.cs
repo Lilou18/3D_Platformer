afc40c6e34159bc91f40355727d9e4fee61c2436
Lilou18 <60436875+Lilou18@users.noreply.github.com>
2024-02-13 16:54:24 -0500

    fix bug for collision and optimize code

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -28,81 +29,66 @@ public class PlayerCollision : MonoBehaviour
     {
         audioSource = GetComponent<AudioSource>();
         playerController = GetComponent<PlayerController>();
-     
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        // When the player touches a coin
-        if (other.gameObject.tag == "coin")
+        switch (other.gameObject.tag)
         {
-            // Instantiate a sound and pick up effect
-            audioSource.PlayOneShot(coinPickUp);
-            GameObject go = Instantiate(pickupEffect, other.transform.position, Quaternion.identity);
-            Destroy(go, 0.5f);
+            case "coin":
+                // When the player touches a coin
+                // Instantiate a sound and pick up effect
+                audioSource.PlayOneShot(coinPickUp);
+                GameObject go = Instantiate(pickupEffect, other.transform.position, Quaternion.identity);
+                Destroy(go, 0.5f);
 
-            PlayerInfos.pi.IncrementCoins(); // Update the number of coins collected
-            Destroy(other.gameObject);       // Destroy the coin
+                PlayerInfos.pi.IncrementCoins(); // Update the number of coins collected
+                Destroy(other.gameObject);       // Destroy the coin
+                break;
+
+            case "water":
+                // The player dies when he falls into the water
+                // Instantiate splash of water effect
+                GameObject water = Instantiate(waterEffect, gameObject.transform.position, Quaternion.identity);
+
+                playerController.isDead = true; // Prevent the player from moving
+                StartCoroutine("RestartScene");
+                break;
+
+            case "fin":
+                // The player completes the level when they reach the top of the tower
+                PlayerInfos.pi.GetScore(); // Display the player's score
+                playerController.isDead = true; // Prevent the player from moving
+                StartCoroutine("NewGame");
+                break;
+
+            //Camera management
+            // When the player is near the tower, we change camera to show all the different sides
+            case "cam1":
+                StartCoroutine(SwitchCamera(cam1, true, 1));
+                break;
+            case "cam2":
+                StartCoroutine(SwitchCamera(cam2, true, 2));
+                break;
+            default: break;
         }
 
-        // The player dies when he falls into the water
-        if(other.gameObject.tag == "water")
-        {
-            // Instantiate splash of water effect
-            GameObject water = Instantiate(waterEffect, gameObject.transform.position, Quaternion.identity);
-            
-            playerController.isDead = true; // Prevent the player from moving
-            StartCoroutine("RestartScene");
-        }
-
-        // The player completes the level when they reach the top of the tower
-        if (other.gameObject.name == "Fin")
-        {
-            PlayerInfos.pi.GetScore(); // Display the player's score
-            playerController.isDead = true; // Prevent the player from moving
-            StartCoroutine("NewGame");
-
-        }
-
-        // Camera management
-        towerColliderEnter = other;
-        Invoke("OnTriggerEnterTower", 0.2f); // Introduce a delay to accommodate the player's movement direction change
-
-    }
-
-    public void OnTriggerEnterTower()
-    {
-        
-        // When the player is near the tower, we change camera to show all the different sides
-        if (towerColliderEnter.gameObject.tag == "cam1")
-        {
-            cam1.SetActive(true);
-            playerController.camActive = 1;
-        }
-        else if (towerColliderEnter.gameObject.tag == "cam2")
-        {
-            cam2.SetActive(true);
-            playerController.camActive = 2;
-        }
     }
 
     // Switch back to the main camera when the player exits the tower zone or is facing the main camera
     private void OnTriggerExit(Collider other)
     {
-        towerColliderExit = other;
-        Invoke("OnTriggerExitTower", 0.2f); // Introduce a delay to accommodate the player's movement direction change
-    }
 
-    public void OnTriggerExitTower()
-    {
-        if (towerColliderExit.gameObject.tag == "cam1")
+        switch (other.gameObject.tag)
         {
-            cam1.SetActive(false);
-            playerController.camActive = 0;
-        }
-        else if (towerColliderExit.gameObject.tag == "cam2")
-        {
-            cam2.SetActive(false);
-            playerController.camActive = 0;
+            case "cam1":
+                StartCoroutine(SwitchCamera(cam1, false, 0));
+                break;
+            case "cam2":
+                StartCoroutine(SwitchCamera(cam2, false, 0));
+                break;
+            default:
+                break;
         }
     }
 
@@ -114,27 +100,27 @@ public class PlayerCollision : MonoBehaviour
         {
             isInvincible = true;                                   // The player becomes invincible for a few seconds
             PlayerInfos.pi.SetHealth(-1);                          // Update the player health
-            iTween.PunchPosition(gameObject, Vector3.back , 0.5f); // Small animation from being hit
+            iTween.PunchPosition(gameObject, Vector3.back, 0.5f); // Small animation from being hit
             StartCoroutine("ResetInvincible");
 
         }
 
         // if the player jump on the enemy, the enemy dies
         if (hit.gameObject.tag == "mob" && canInstantiateParticle)
-        {            
-            hit.gameObject.transform.parent.GetComponent<Collider>().enabled = false;                         
+        {
+            hit.gameObject.transform.parent.GetComponent<Collider>().enabled = false;
             iTween.PunchScale(hit.gameObject.transform.parent.gameObject, new Vector3(20f, 20f, 20f), 0.6f);  // Animation of the enemy being hit
 
-            canInstantiateParticle = false; 
+            canInstantiateParticle = false;
             GameObject go = Instantiate(mobpEffect, hit.transform.position, Quaternion.identity);             // Instantiate an effect when an enemy dies
             Destroy(go, 0.6f);
 
-            Instantiate(loot, hit.gameObject.transform.position + Vector3.forward, Quaternion.identity * Quaternion.Euler(90,0,0)); // Generate a coin to reward the player
+            Instantiate(loot, hit.gameObject.transform.position + Vector3.forward, Quaternion.identity * Quaternion.Euler(90, 0, 0)); // Generate a coin to reward the player
 
             audioSource.PlayOneShot(hitSound);
             StartCoroutine("ResetInstantiate");
             Destroy(hit.gameObject.transform.parent.gameObject, 0.5f); // Destroy the enemy
-                        
+
         }
 
         // The player dies when he falls out of the level area
@@ -146,18 +132,26 @@ public class PlayerCollision : MonoBehaviour
 
     }
 
+    // Activate the correct camera after a short delay to enssure smooth transition
+    IEnumerator SwitchCamera(GameObject cam, bool isActive, int camActive)
+    {
+        cam.SetActive(isActive);
+        yield return new WaitForSeconds(0.2f);
+        playerController.camActive = camActive;
+    }
+
     // The player has completeted the game, we activate the fireworks and then we restart the game
     IEnumerator NewGame()
     {
         fireworks.SetActive(true);
         yield return new WaitForSeconds(7);
-         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     // Restart the level from the beginning
     IEnumerator RestartScene()
-    {        
-        yield return new WaitForSeconds(0.75f);        
+    {
+        yield return new WaitForSeconds(0.75f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
@@ -169,10 +163,10 @@ public class PlayerCollision : MonoBehaviour
     }
 
     // When the player is hit by an enemy, they become invincible for a few seconds
-    IEnumerator ResetInvincible() 
+    IEnumerator ResetInvincible()
     {
         // Blinking effect on the player
-        for(int i = 0; i < 6; i++)
+        for (int i = 0; i < 6; i++)
         {
             yield return new WaitForSeconds(0.2f);
             rend.enabled = !rend.enabled;
@@ -180,7 +174,7 @@ public class PlayerCollision : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         rend.enabled = true;
         isInvincible = false;
-    
+
     }
 
 }
